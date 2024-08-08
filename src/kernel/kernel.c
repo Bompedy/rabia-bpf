32b12906e393f2f2e9aaa24c383aed292a0a7296
money <lucasphone75@gmail.com>
2024-08-08 13:41:41 -0600

    debug interface index

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,25 +2,19 @@
 #include <bpf/bpf_helpers.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-//struct bpf_map_def SEC("maps") packet_count_map = {
-//        .type = BPF_MAP_TYPE_ARRAY,
-//        .key_size = sizeof(__u32),
-//        .value_size = sizeof(__u64),
-//        .max_entries = 1,
-//};
-//
-//SEC("xdp")
-//int xdp_prog(struct __sk_buff *skb) {
-//    int key = 0;
-//    long *value = bpf_map_lookup_elem(&packet_count_map, &key);
-//    if (value) __sync_fetch_and_add(value, 1);
-//    return XDP_DROP;
-//}
-
+struct bpf_map_def SEC("maps") packet_count_map = {
+        .type = BPF_MAP_TYPE_ARRAY,
+        .key_size = sizeof(__u32),
+        .value_size = sizeof(__u64),
+        .max_entries = 1,
+};
 
 SEC("xdp")
 int xdp_prog(struct __sk_buff *skb) {
-    return XDP_PASS;
+    int key = 0;
+    long *value = bpf_map_lookup_elem(&packet_count_map, &key);
+    if (value) __sync_fetch_and_add(value, 1);
+    return XDP_DROP;
 }
 
 char _license[] SEC("license") = "GPL";
