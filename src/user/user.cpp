0e13fab1c9dfc5fd81cbf4d481aac2176572f23d
money <lucasphone75@gmail.com>
2024-08-28 16:17:58 -0600

    add xdp print

--- src/user/user.cpp
+++ src/user/user.cpp
@@ -142,7 +142,9 @@ int main() {
     std::cout << "Got log ring: " << log_ring << std::endl;
     std::thread thread([&]() {
         while (true) {
+            std::cout << "poll it" << std::endl;
             const auto error = ring_buffer__poll(log_ring, 200);
+            std::cout << "got nothing" << std::endl;
             if (error < 0) std::cerr << "Error polling!" << std::endl;
         }
     });
