a381afb43e5c70d257f88131d38874f6707397fd
money <lucasphone75@gmail.com>
2024-08-28 16:21:26 -0600

    test debugging

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -7,6 +7,13 @@ struct {
     __uint(max_entries, 4096);
 } output_buf SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, int);
+    __type(value, int);
+    __uint(max_entries, 1);
+} counter_map SEC(".maps");
+
 
 //struct {
 //    __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -29,8 +36,26 @@ char max_addresses = 0;
 
 SEC("xdp")
 int xdp_hook(struct __sk_buff* skb) {
-    int error;
     int index = 0;
+    int key = 0;
+    int *counter;
+
+    // Look up the counter value
+
+    counter = bpf_map_lookup_elem(&counter_map, &key);
+    if (counter) {
+        // Increment the counter
+        (*counter)++;
+        // Optionally print the new counter value to the ring buffer
+        char message[64];
+        __builtin_snprintf(message, sizeof(message), "Counter: %d", *counter);
+        print(message);
+    } else {
+        // Optionally handle the case where the counter is not found
+        print("Counter map lookup failed");
+    }
+
+
     print("hello");
 //    bpf_map_lookup_elem(&addresses, 0);
 //    while (error == 0) {
