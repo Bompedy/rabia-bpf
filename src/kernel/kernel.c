e20bb79e9bdcbc4aabf0f9563a9ca680e29d7e10
money <lucasphone75@gmail.com>
2024-08-30 16:25:28 -0600

    debug source mac addr

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -23,6 +23,10 @@ int xdp_hook(struct xdp_md* ctx) {
     void *data_end = (void *)(long)ctx->data_end;
     __u32 len = data_end - data;
     bpf_printk("\nPacket %u: ", len);
+    struct ethhdr *eth = (struct ethhdr *) data;
+    bpf_printk("Source MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth->h_source[0], eth->h_source[1], eth->h_source[2],
+               eth->h_source[3], eth->h_source[4], eth->h_source[5]);
     return XDP_PASS;
 }
 
