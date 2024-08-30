99f45d1d844c68dbaec7fd33115b5a31f640239f
money <lucasphone75@gmail.com>
2024-08-30 16:02:01 -0600

    debug source mac addr

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -24,51 +24,12 @@ int xdp_hook(struct __sk_buff* skb) {
         return XDP_PASS;
     }
     struct ethhdr *eth = (struct ethhdr *)(long)skb->data;
-    __u64 data[ETH_ALEN];
-    int len = 0;
 
+    bpf_printk("\nPacket %d: ", skb->len);
     for (int i = 0; i < ETH_ALEN; ++i) {
-        data[i] = eth->h_source[i];
+        bpf_printk("Source MAC address: %d", eth->h_source[i]);
     }
-    char mac_str[18];
 
-    bpf_snprintf(mac_str, sizeof(mac_str), "%02x:%02x:%02x:%02x:%02x:%02x", data, sizeof(data));
-    bpf_printk("Source MAC address: %s\n", mac_str);
-
-//    print("Packet: ");
-//    if (skb->len < sizeof(struct ethhdr)) {
-//        print("Not a packet we want!");
-//        return XDP_PASS;
-//    }
-//
-//    struct ethhdr *eth = (struct ethhdr *)(long)skb->data;
-//    print("Converted to eth header!");
-//
-////    struct ethhdr eth;
-////    if (bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth)) < 0) {
-//////        print("couldn't load bytes!");
-////        return XDP_PASS;
-////    }
-//
-//    char mac_str[18];
-//    __u64 data[ETH_ALEN];
-//    int len = 0;
-//
-//    for (int i = 0; i < ETH_ALEN; ++i) {
-//        data[i] = eth->h_source[i];
-//    }
-//
-////    len = bpf_snprintf(mac_str, sizeof(mac_str),
-////                       "%02x:%02x:%02x:%02x:%02x:%02x",
-////                       data, sizeof(__u64) * ETH_ALEN);
-////
-////    // Ensure null-termination
-////    mac_str[len] = '\0';
-////
-////
-////    print(mac_str);
-//
-//    print("Complete!");
     return XDP_PASS;
 }
 
