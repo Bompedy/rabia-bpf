98464523d4864fa2a075dc593a82eed2b39dc346
money <lucasphone75@gmail.com>
2024-08-30 15:52:15 -0600

    xdp debugs

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -19,40 +19,41 @@ unsigned long commit_index = 0L;
 
 SEC("xdp")
 int xdp_hook(struct __sk_buff* skb) {
+    bpf_printk("test print");
 //    print("Packet: ");
-    if (skb->len < sizeof(struct ethhdr)) {
-        print("Not a packet we want!");
-        return XDP_PASS;
-    }
-
-    struct ethhdr *eth = (struct ethhdr *)(long)skb->data;
-    print("Converted to eth header!");
-
-//    struct ethhdr eth;
-//    if (bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth)) < 0) {
-////        print("couldn't load bytes!");
+//    if (skb->len < sizeof(struct ethhdr)) {
+//        print("Not a packet we want!");
 //        return XDP_PASS;
 //    }
-
-    char mac_str[18];
-    __u64 data[ETH_ALEN];
-    int len = 0;
-
-    for (int i = 0; i < ETH_ALEN; ++i) {
-        data[i] = eth->h_source[i];
-    }
-
-//    len = bpf_snprintf(mac_str, sizeof(mac_str),
-//                       "%02x:%02x:%02x:%02x:%02x:%02x",
-//                       data, sizeof(__u64) * ETH_ALEN);
 //
-//    // Ensure null-termination
-//    mac_str[len] = '\0';
+//    struct ethhdr *eth = (struct ethhdr *)(long)skb->data;
+//    print("Converted to eth header!");
 //
+////    struct ethhdr eth;
+////    if (bpf_skb_load_bytes(skb, 0, &eth, sizeof(eth)) < 0) {
+//////        print("couldn't load bytes!");
+////        return XDP_PASS;
+////    }
 //
-//    print(mac_str);
-
-    print("Complete!");
+//    char mac_str[18];
+//    __u64 data[ETH_ALEN];
+//    int len = 0;
+//
+//    for (int i = 0; i < ETH_ALEN; ++i) {
+//        data[i] = eth->h_source[i];
+//    }
+//
+////    len = bpf_snprintf(mac_str, sizeof(mac_str),
+////                       "%02x:%02x:%02x:%02x:%02x:%02x",
+////                       data, sizeof(__u64) * ETH_ALEN);
+////
+////    // Ensure null-termination
+////    mac_str[len] = '\0';
+////
+////
+////    print(mac_str);
+//
+//    print("Complete!");
     return XDP_PASS;
 }
 
