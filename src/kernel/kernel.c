53c1ff2450cda2aa98fa08c64568f6c76486b88b
money <lucasphone75@gmail.com>
2024-08-30 22:36:26 -0600

    redirect packets

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -30,36 +30,37 @@ int interface_index;
 
 SEC("xdp")
 int xdp_hook(struct xdp_md *ctx) {
-    void *data = (void *) (long) ctx->data;
-    void *data_end = (void *) (long) ctx->data_end;
-    if (data + sizeof(struct ethhdr) > data_end) {
-        return XDP_DROP;
-    }
-    struct ethhdr *in_eth = (struct ethhdr *) data;
+//    void *data = (void *) (long) ctx->data;
+//    void *data_end = (void *) (long) ctx->data_end;
+//    if (data + sizeof(struct ethhdr) > data_end) {
+//        return XDP_DROP;
+//    }
+//    struct ethhdr *in_eth = (struct ethhdr *) data;
+    bpf_printk("test");
 
 
-    for (int i = 0; i < 3; ++i) {
-        unsigned char* address = addresses[i];
-        if (
-                address[0] != machine_address[0] && address[1] != machine_address[1] && address[2] != machine_address[2]
-                && address[3] != machine_address[3] && address[4] != machine_address[4] &&
-                address[5] != machine_address[5]
-        ) {
-            in_eth->h_dest[0] = address[0];
-            in_eth->h_dest[1] = address[1];
-            in_eth->h_dest[2] = address[2];
-            in_eth->h_dest[3] = address[3];
-            in_eth->h_dest[4] = address[4];
-            in_eth->h_dest[5] = address[5];
-            int result = bpf_clone_redirect(ctx, ctx->ingress_ifindex, 0);
-            if (result) {
-                bpf_printk("Dropping packet!");
-                return XDP_DROP;
-            } else {
-                bpf_printk("Sent packet to %d!", i);
-            }
-        }
-    }
+//    for (int i = 0; i < 3; ++i) {
+//        unsigned char* address = addresses[i];
+//        if (
+//                address[0] != machine_address[0] && address[1] != machine_address[1] && address[2] != machine_address[2]
+//                && address[3] != machine_address[3] && address[4] != machine_address[4] &&
+//                address[5] != machine_address[5]
+//        ) {
+//            in_eth->h_dest[0] = address[0];
+//            in_eth->h_dest[1] = address[1];
+//            in_eth->h_dest[2] = address[2];
+//            in_eth->h_dest[3] = address[3];
+//            in_eth->h_dest[4] = address[4];
+//            in_eth->h_dest[5] = address[5];
+//            int result = bpf_clone_redirect(ctx, ctx->ingress_ifindex, 0);
+//            if (result) {
+//                bpf_printk("Dropping packet!");
+//                return XDP_DROP;
+//            } else {
+//                bpf_printk("Sent packet to %d!", i);
+//            }
+//        }
+//    }
 
     return XDP_PASS;
 }
