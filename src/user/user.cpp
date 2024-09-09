cf1ce9e3c61aa1f6ac78585a6c16ed301cff84f7
Bompedy <lucasphone75@gmail.com>
2024-09-09 17:01:24 -0600

    initialize pipes

--- src/user/user.cpp
+++ src/user/user.cpp
@@ -249,7 +249,7 @@ int main() {
             return EXIT_FAILURE;
         }
 
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+        std::this_thread::sleep_for(std::chrono::seconds(10));
         send_packet(sock_write, machine_address.mac, BROADCAST, &INIT, 1);
 
 
