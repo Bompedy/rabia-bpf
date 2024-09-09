005b212c25a13ef6c94622b3e576ce20b8774d83
Bompedy <lucasphone75@gmail.com>
2024-09-09 15:03:43 -0600

    remove qdisc first

--- run.sh
+++ run.sh
@@ -5,6 +5,16 @@ mkdir -p build
 cd build
 export INTERFACE=enp4s0f1
 
+existing_qdisc=$(sudo tc qdisc show dev $INTERFACE | grep clsact)
+if [ ! -z "$existing_qdisc" ]; then
+    echo "Removing existing qdisc from $INTERFACE"
+    sudo tc qdisc del dev $INTERFACE clsact
+    if [ $? -ne 0 ]; then
+        echo "Failed to remove existing qdisc from $INTERFACE"
+        exit 1
+    fi
+fi
+
 sudo tc qdisc add dev $INTERFACE clsact
 if [ $? -ne 0 ]; then
     echo "Failed to add qdisc to $INTERFACE"
