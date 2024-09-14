d72b02fea36d9cc552d2e85bc6b2477334747b50
Bompedy <lucasphone75@gmail.com>
2024-09-14 00:58:52 -0600

    proposals and acks

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,36 +3,29 @@
 #include <linux/if_ether.h>
 #include <linux/pkt_cls.h>
 
-struct {
-    __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 4096);
-} output_buf SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 4096);
-} request_buf SEC(".maps");
-
-#define NUM_PIPES 20
-
 #define MENCIUS_REVISITED 0
 #define PAXOS_HELPER 0
 #define MULTI_PAXOS 1
-#define MAJORITY 2
 
-const unsigned char INIT = 0;
-const unsigned char PROPOSE = 1;
-const unsigned char ACK = 2;
+#define INIT 0
+#define PROPOSE 1
+#define ACK 2
 
-unsigned int consumed = 0;
-unsigned long long committed = 0;
-unsigned long long acks[1000000];
+#define NUM_PIPES 20
+#define LOG_SIZE 50000
+#define DATA_SIZE 1400
+#define NODES 3
+#define MAJORITY (NODES/2 + 1)
 
 
 unsigned char machine_address[6];
-unsigned char addresses[3][6];
+unsigned char addresses[NODES][6];
 unsigned char node_index;
-char paxos_log[500][1400];
+
+char data_log[LOG_SIZE][DATA_SIZE];
+unsigned long long acks[LOG_SIZE];
+unsigned long long committed = 0;
+unsigned long long consumed = 0;
 
 struct paxos_hdr {
     unsigned char op;
@@ -40,6 +33,8 @@ struct paxos_hdr {
     unsigned long long next;
     int data_size;
 };
+
+const int HEADER_SIZE = (sizeof(struct ethhdr) + sizeof(struct paxos_hdr));
 
 unsigned short htons(unsigned short value) {
     unsigned short result;
@@ -56,7 +51,6 @@ unsigned short htons(unsigned short value) {
     return result;
 }
 
-const unsigned int HEADER_SIZE = (sizeof(struct ethhdr) + sizeof(struct paxos_hdr));
 
 SEC("xdp")
 int xdp_hook(struct xdp_md *ctx) {
@@ -73,11 +67,10 @@ int xdp_hook(struct xdp_md *ctx) {
             while (in_paxos->next > current && __sync_val_compare_and_swap(&committed, current, in_paxos->next) == current);
 
             if (in_paxos->data_size > 0 && in_paxos->data_size < 1400 &&
-                bpf_skb_load_bytes((char *) (data + HEADER_SIZE), 0,paxos_log[in_paxos->slot], in_paxos->data_size) < 0
+                    bpf_skb_load_bytes((char *) (data + HEADER_SIZE), 0, data_log[in_paxos->slot], in_paxos->data_size) < 0
             ) {
-                bpf_printk("ERRORED WHEN LOADING BYTES!");
+                bpf_printk("ERRORED WHEN STORING BYTES!");
             } else {
-                bpf_printk("successfuly stored bytes!");
                 for (int i = 0; i < 6; ++i) {
                     in_eth->h_dest[i] = MULTI_PAXOS ? in_eth->h_source[i] : addresses[0][i];
                     in_eth->h_source[i] = machine_address[i];
@@ -93,9 +86,8 @@ int xdp_hook(struct xdp_md *ctx) {
                 unsigned long long current;
                 in_paxos->next = current = __sync_fetch_and_add(&committed, 0);
                 for (unsigned long long i = current + 1; i <= slot; ++i) {
-                    if (__sync_fetch_and_add(&acks[i], 0) >= MAJORITY) {
-                        in_paxos->next = i;
-                    } else break;
+                    if (__sync_fetch_and_add(&acks[i], 0) >= MAJORITY) in_paxos->next = i;
+                    else break;
                 }
 
                 while (in_paxos->next > current && __sync_val_compare_and_swap(&committed, current, in_paxos->next) == current) {
@@ -133,7 +125,7 @@ int tc_hook(struct __sk_buff *skb) {
         struct paxos_hdr *in_paxos = (struct paxos_hdr*) ((unsigned char *)data + sizeof(struct ethhdr));
         if (in_paxos->op == INIT) {
             in_paxos->op = PROPOSE;
-            in_paxos->data_size = 4;
+            in_paxos->data_size = -1; // for skip?
             unsigned long long slot = in_paxos->slot;
             if ((MULTI_PAXOS && node_index == 0) || (PAXOS_HELPER && node_index % slot == 0)) {
                 // poll or skip, update our log
