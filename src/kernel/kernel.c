24f50bc81d0cdcf3f55d7ea41d69c0fd2c6e4c53
Bompedy <lucasphone75@gmail.com>
2024-09-13 23:59:20 -0600

    add paxos header

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -41,9 +41,7 @@ unsigned int acks[1000000];
 unsigned char machine_address[6];
 unsigned char addresses[3][6];
 unsigned char node_index;
-
-
-#define print(message) bpf_ringbuf_output(&output_buf, message, sizeof(message), 0)
+char paxos_log[500][1400];
 
 struct paxos_hdr {
     unsigned char op;
@@ -67,6 +65,9 @@ unsigned short htons(unsigned short value) {
     return result;
 }
 
+void memcpy(char* d, char* s, int count) {
+
+}
 
 SEC("xdp")
 int xdp_hook(struct xdp_md *ctx) {
@@ -76,92 +77,20 @@ int xdp_hook(struct xdp_md *ctx) {
     struct ethhdr *in_eth = (struct ethhdr *) data;
     if (in_eth->h_proto == htons(0xD0D0)) {
         struct paxos_hdr *in_paxos = (struct paxos_hdr*) ((unsigned char *) data + sizeof(struct ethhdr));
-        bpf_printk("GOT PIPE SETUP PACKET: op=%d, slot=%d, next=%d, data_size=%d", in_paxos->op, in_paxos->slot, in_paxos->next, in_paxos->data_size);
+        int skip = in_paxos->data_size == -1 ? 1 : 0;
+        if (!skip) {
+            if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) + in_paxos->data_size > data_end) return XDP_PASS;
+            if (bpf_skb_load_bytes((char*) (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr)), 0, paxos_log[in_paxos->slot], in_paxos->data_size) < 0) {
+                bpf_printk("ERRORED WHEN LOADING BYTES!");
+            } else {
+                bpf_printk("successfuly stored bytes!");
+            }
+//            __builtin_memcpy(paxos_log[in_paxos->slot], (char*) (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr)), in_paxos->data_size);
+        }
+//        bpf_printk("GOT PIPE SETUP PACKET: op=%d, slot=%d, next=%d, data_size=%d", in_paxos->op, in_paxos->slot, in_paxos->next, in_paxos->data_size);
         return XDP_PASS;
     }
 
-//    unsigned char *op = ((unsigned char *)data + sizeof(struct ethhdr));
-//    unsigned int *slot = ((unsigned int *)data + sizeof(struct ethhdr) + 1);
-//    if (*op == PROPOSE) {
-//        unsigned int next = *((unsigned int *)data + sizeof(struct ethhdr) + 5);
-//        int current;
-//        do {
-//            current = committed;
-//        } while (next > current && !__sync_val_compare_and_swap(&committed, current, next));
-//        if (PAXOS_HELPER) {
-//            *op = ACK;
-//            for (int i = 0; i < 6; i++) {
-//                in_eth->h_source[i] = machine_address[i];
-//                in_eth->h_dest[i] = addresses[0][i];
-//            }
-//
-//        }
-//    } else if (*op == ACK) {
-//
-//    }
-
-//    if (op == 0) { //message
-//        unsigned char* address = addresses[0];
-//        if (
-//                address[0] != machine_address[0] && address[1] != machine_address[1] && address[2] != machine_address[2]
-//                && address[3] != machine_address[3] && address[4] != machine_address[4] && address[5] != machine_address[5]
-//        ) { //forward
-//            for (int i = 0; i < 6; i++) {
-//                in_eth->h_source[i] = machine_address[i];
-//                in_eth->h_dest[i] = address[i];
-//            }
-//            return XDP_TX;
-//        } else { //leader
-//            int index = __sync_add_and_fetch(&commit_index, 1);
-//            for (int i = 0; i < 6; i++) {
-//                in_eth->h_source[i] = machine_address[i];
-//                in_eth->h_dest[i] = 0xFF;
-//            }
-//            *((char*) (data + sizeof(struct ethhdr))) = 1;
-//            *((int*) (data + sizeof(struct ethhdr) + 1)) = index;
-//            return XDP_TX;
-//        }
-//    } else if (op == 1) {
-//        for (int i = 0; i < 6; i++) {
-//            in_eth->h_source[i] = machine_address[i];
-//            in_eth->h_dest[i] = addresses[0][i];
-//        }
-//        *((char*) (data + sizeof(struct ethhdr))) = 2;
-//        return XDP_TX;
-//    } else if (op == 2) {
-//        unsigned long index = *((char*) (data + sizeof(struct ethhdr) + 1));
-////        unsigned int ack_count = __sync_add_and_fetch(&acks[index], 1);
-////        if (ack_count >= 2 && commit_index + 1 == index) while (acks[index] >= 2) {
-////                //log index
-////                __sync_val_compare_and_swap(&commit_index, index - 1, index);
-////                index++;
-////            }
-//        return XDP_PASS;
-//    }
-
-
-//    for (int i = 0; i < 3; ++i) {
-//        unsigned char* address = addresses[i];
-//        if (
-//                address[0] != machine_address[0] && address[1] != machine_address[1] && address[2] != machine_address[2]
-//                && address[3] != machine_address[3] && address[4] != machine_address[4] &&
-//                address[5] != machine_address[5]
-//        ) {
-//            in_eth->h_dest[0] = address[0];
-//            in_eth->h_dest[1] = address[1];
-//            in_eth->h_dest[2] = address[2];
-//            in_eth->h_dest[3] = address[3];
-//            in_eth->h_dest[4] = address[4];
-//            in_eth->h_dest[5] = address[5];
-//            int result = bpf_clone_redirect(ctx, ctx->ingress_ifindex, 0);
-//            if (result) {
-//                bpf_printk("Dropping packet!");
-//                return XDP_DROP;
-//            } else {
-//                bpf_printk("Sent packet to %d!", i);
-//            }
-//        }
-//    }
 
     return XDP_PASS;
 }
@@ -203,32 +132,7 @@ on_ack(slot: int) {
   //if we own the slot (MP owns no matter what) then propose next and let learn.
   if (MULTI_PAXOS || slot % THIS_NODE == 0)
     multicast_propose(pq.poll() ?? SKIP, slot += NUM_PIPES, next)
-}
-
-
-//MP has all pipes on leader, PH and MR distribute them evenly.
-for (i in NUM_PIPES)
-  if (MULTI_PAOXS || i % THIS_NODE == 0)
-     multicast_propose(pq.poll() ?? SKIP, i, 0)
-*/
-
-// eth header
-// sender 6
-// receiver 6
-// protocol 2
-
-// 1400 - 35
-// 1365
-
-// consensus header
-// op = INIT, PROPOSE, ACK 1
-// next = committed up to 8
-// slot 8
-// bytes size 4
-// bytes message undefined
-
-
-int initialized_pipes;
+}*/
 
 SEC("tc")
 int tc_hook(struct __sk_buff *skb) {
@@ -240,6 +144,7 @@ int tc_hook(struct __sk_buff *skb) {
         struct paxos_hdr *in_paxos = (struct paxos_hdr*) ((unsigned char *)data + sizeof(struct ethhdr));
         if (in_paxos->op == INIT) {
             in_paxos->op = PROPOSE;
+            in_paxos->data_size = 4;
             unsigned long long slot = in_paxos->slot;
             if (MULTI_PAXOS && node_index == 0) {
                 if (bpf_clone_redirect(skb, skb->ifindex, 0)) {
