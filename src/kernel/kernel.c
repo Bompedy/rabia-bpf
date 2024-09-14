337b85cd590455bd4ade49e1208bb2e7fe1af56b
Bompedy <lucasphone75@gmail.com>
2024-09-13 23:10:46 -0600

    add paxos header

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -72,13 +72,12 @@ SEC("xdp")
 int xdp_hook(struct xdp_md *ctx) {
     void *data = (void *) (long) ctx->data;
     void *data_end = (void *) (long) ctx->data_end;
-    if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) {
-        return XDP_DROP;
-    }
+    if (data + sizeof(struct ethhdr) > data_end) return XDP_DROP;
     struct ethhdr *in_eth = (struct ethhdr *) data;
     if (in_eth->h_proto == htons(0xD0D0)) {
+        if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) return XDP_DROP;
         struct paxos_hdr *in_paxos = (struct paxos_hdr*) ((unsigned char *) data + sizeof(struct ethhdr));
-        bpf_printk("GOT PIPE SETUP PACKET: op=%d, slot=%d, next=%d, data_size=%d", in_paxos->op, in_paxos->slot, in_paxos->next, in_paxos->data_size);
+//        bpf_printk("GOT PIPE SETUP PACKET: op=%d, slot=%d, next=%d, data_size=%d", in_paxos->op, in_paxos->slot, in_paxos->next, in_paxos->data_size);
         return XDP_PASS;
     }
 
@@ -229,13 +228,76 @@ for (i in NUM_PIPES)
 // bytes size 4
 // bytes message undefined
 
+/*libbpf: prog 'tc_hook': BPF program load failed: Permission denied
+libbpf: prog 'tc_hook': -- BEGIN PROG LOAD LOG --
+0: R1=ctx(off=0,imm=0) R10=fp0
+; int tc_hook(struct __sk_buff *skb) {
+0: (bf) r6 = r1                       ; R1=ctx(off=0,imm=0) R6_w=ctx(off=0,imm=0)
+1: (b7) r0 = 0                        ; R0_w=0
+; void *data_end = (void *) (long) skb->data_end;
+2: (61) r1 = *(u32 *)(r6 +80)         ; R1_w=pkt_end(off=0,imm=0) R6_w=ctx(off=0,imm=0)
+; void *data = (void *) (long) skb->data;
+3: (61) r8 = *(u32 *)(r6 +76)         ; R6_w=ctx(off=0,imm=0) R8_w=pkt(off=0,r=0,imm=0)
+; if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) return TC_ACT_OK;
+4: (bf) r2 = r8                       ; R2_w=pkt(off=0,r=0,imm=0) R8_w=pkt(off=0,r=0,imm=0)
+5: (07) r2 += 46                      ; R2_w=pkt(off=46,r=0,imm=0)
+; if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) return TC_ACT_OK;
+6: (2d) if r2 > r1 goto pc+14         ; R1_w=pkt_end(off=0,imm=0) R2_w=pkt(off=46,r=46,imm=0)
+; if (in_eth->h_proto == htons(0xD0D0)) {
+7: (71) r1 = *(u8 *)(r8 +12)          ; R1_w=scalar(umax=255,var_off=(0x0; 0xff)) R8_w=pkt(off=0,r=46,imm=0)
+8: (71) r2 = *(u8 *)(r8 +13)          ; R2_w=scalar(umax=255,var_off=(0x0; 0xff)) R8_w=pkt(off=0,r=46,imm=0)
+9: (67) r2 <<= 8                      ; R2_w=scalar(umax=65280,var_off=(0x0; 0xff00))
+10: (4f) r2 |= r1                     ; R1_w=scalar(umax=255,var_off=(0x0; 0xff)) R2_w=scalar()
+; if (in_eth->h_proto == htons(0xD0D0)) {
+11: (55) if r2 != 0xd0d0 goto pc+9    ; R2_w=53456
+; if (in_paxos->op == INIT) {
+12: (71) r1 = *(u8 *)(r8 +14)         ; R1=scalar(umax=255,var_off=(0x0; 0xff)) R8=pkt(off=0,r=46,imm=0)
+; if (in_paxos->op == INIT) {
+13: (55) if r1 != 0x0 goto pc+7       ; R1=0
+14: (b7) r1 = 1                       ; R1_w=1
+; in_paxos->op = PROPOSE;
+15: (73) *(u8 *)(r8 +14) = r1         ; R1_w=1 R8=pkt(off=0,r=46,imm=0)
+16: (b7) r7 = 0                       ; R7_w=0
+17: (05) goto pc+4
+; in_paxos->slot = i;
+22: (7b) *(u64 *)(r8 +22) = r7        ; R7_w=0 R8=pkt(off=0,r=46,imm=0)
+; if (bpf_clone_redirect(skb, skb->ifindex, 0)) {
+23: (61) r2 = *(u32 *)(r6 +40)        ; R2_w=scalar(umax=4294967295,var_off=(0x0; 0xffffffff)) R6=ctx(off=0,imm=0)
+24: (bf) r1 = r6                      ; R1_w=ctx(off=0,imm=0) R6=ctx(off=0,imm=0)
+25: (b7) r3 = 0                       ; R3_w=0
+26: (85) call bpf_clone_redirect#13   ; R0=scalar()
+; if (bpf_clone_redirect(skb, skb->ifindex, 0)) {
+27: (15) if r0 == 0x0 goto pc-10      ; R0=scalar()
+; bpf_printk("FAILED PIPE INIT: %d", i);
+28: (18) r1 = 0xffff90294bfe994d      ; R1_w=map_value(off=61,ks=4,vs=82,imm=0)
+30: (b7) r2 = 21                      ; R2_w=21
+31: (bf) r3 = r7                      ; R3_w=0 R7=0
+32: (85) call bpf_trace_printk#6      ; R0_w=scalar()
+33: (05) goto pc-16
+; in_paxos->op = PROPOSE;
+18: (b7) r0 = 2                       ; R0_w=2
+; for (int i = 0; i < NUM_PIPES; ++i) {
+19: (07) r7 += 1                      ; R7=1
+; for (int i = 0; i < NUM_PIPES; ++i) {
+20: (55) if r7 != 0x14 goto pc+1      ; R7=1
+; in_paxos->slot = i;
+22: (7b) *(u64 *)(r8 +22) = r7
+R8 invalid mem access 'scalar'
+processed 33 insns (limit 1000000) max_states_per_insn 0 total_states 3 peak_states 3 mark_read 2
+-- END PROG LOAD LOG --
+libbpf: prog 'tc_hook': failed to load: -13
+libbpf: failed to load object 'kernel'
+libbpf: failed to load BPF skeleton 'kernel': -13
+Failed to load bpf skeleton*/
+
 SEC("tc")
 int tc_hook(struct __sk_buff *skb) {
     void *data = (void *) (long) skb->data;
     void *data_end = (void *) (long) skb->data_end;
-    if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) return TC_ACT_OK;
+    if (data + sizeof(struct ethhdr) > data_end) return TC_ACT_OK;
     struct ethhdr *in_eth = (struct ethhdr *) data;
     if (in_eth->h_proto == htons(0xD0D0)) {
+        if (data + sizeof(struct ethhdr) + sizeof(struct paxos_hdr) > data_end) return TC_ACT_OK;
         struct paxos_hdr *in_paxos = (struct paxos_hdr*) ((unsigned char *)data + sizeof(struct ethhdr));
         if (in_paxos->op == INIT) {
             in_paxos->op = PROPOSE;
