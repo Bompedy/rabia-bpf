bbfbc1d8ba46ef3e0d40655897082a1844f3e803
Bompedy <lucasphone75@gmail.com>
2024-09-13 23:17:19 -0600

    add paxos header

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -305,7 +305,7 @@ int tc_hook(struct __sk_buff *skb) {
                 for (int i = 0; i < NUM_PIPES; ++i) {
                     in_paxos->slot = i;
                     if (bpf_clone_redirect(skb, skb->ifindex, 0)) {
-                        bpf_printk("FAILED PIPE INIT: %d", i);
+//                        bpf_printk("FAILED PIPE INIT: %d", i);
                     }
                 }
             }
